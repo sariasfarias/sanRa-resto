66185d28e46bf309928e0594bdebfffb133edbd4
stefania arias <you@example.com>
2020-06-25 23:47:36 -0300

    addind id to serializers

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -7,28 +7,28 @@ from restaurant.models import Restaurant, MenuItem, Reservation
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id', 'url', 'username', 'email', 'groups']
 
 
 class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ['name', 'description', 'address']
+        fields = ['id', 'name', 'description', 'address']
 
 
 class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['name', 'description', 'price', 'restaurant']
+        fields = ['id', 'name', 'description', 'price', 'restaurant']
 
 
 class ReserveByHourSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['capacity', 'currently_free', 'hour', 'restaurant']
+        fields = ['id', 'capacity', 'currently_free', 'hour', 'restaurant']
 
 
 class ReservationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Reservation
-        fields = ['coming', 'duration', 'guest', 'restaurant', 'number_guest']
+        fields = ['id', 'coming', 'duration', 'guest', 'restaurant', 'number_guest']
