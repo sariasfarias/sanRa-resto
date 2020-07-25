1b11b2d6bb6b83d79c8ae4955dc66c2456731fa8
stefania arias <you@example.com>
2020-07-25 19:16:37 -0300

    adding enviroment var

--- project/settings/production.py
+++ project/settings/production.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'o7%(6*rg=d-v8@5_l)igqxch+k%s$ftfanycsq0it)m3d_f7y^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sanra-resto-qa.herokuapp.com', 'sanrafael-resto.herokuapp.com']
+ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(' ')
 
 
 # Application definition
